3ebdfccf1ada618a407c045ee5392d44f21628b1
CharGardner <char.j.gardner@gmail.com>
2024-10-22 08:56:06 -0500

    dispatch101towing

---
+++ story_content/triggers.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5vMy9Em4uVN":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6i6CTw6O2jo');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6j8R5WsjC21';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
